b485f2ed20f279262c5c9222b2f9c13f373fe30f
Corso Montuori <137323676+Corsionio@users.noreply.github.com>
2025-05-26 13:15:43 -0400

    Bug in project, need ',' in staticfiles_dir

--- project/settings.py
+++ project/settings.py
@@ -95,7 +95,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'quiz'/'static'] 
+STATICFILES_DIRS = [BASE_DIR / 'quiz'/'static',] 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
